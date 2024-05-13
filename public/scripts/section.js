b3092b9416730988a3d76c90283f175d8a98ae86
Brianne <92082302+briannededeugd@users.noreply.github.com>
2024-05-13 22:06:34 +0200

    FEAT: Finish goals and plans carousel

--- public/scripts/section.js
+++ public/scripts/section.js
@@ -19,3 +19,59 @@ if (scrollToContent) {
 		window.scrollBy(0, window.innerHeight);
 	});
 }
+
+/**======================
+ *    SCROLL RIGHT
+ *========================**/
+var scrollHorizontalButtons = document.querySelectorAll(".scrollhorizontal");
+scrollHorizontalButtons.forEach((button) => {
+	button.addEventListener("click", scrollHorizontal); // Pass the function reference without invoking it
+});
+
+function scrollHorizontal() {
+	var goalsContainer = document.getElementById("goals");
+
+	if (goalsContainer) {
+		goalsContainer.scrollBy({
+			left: goalsContainer.clientWidth,
+			behavior: "smooth", // Adding smooth scrolling behavior
+		});
+	}
+}
+
+/**======================
+ *    SCROLL LEFT
+ *========================**/
+
+var scrollHorizontalReverseButtons = document.querySelectorAll(".scrollleft");
+scrollHorizontalReverseButtons.forEach((button) => {
+	button.addEventListener("click", scrollHorizontalReversed);
+});
+
+function scrollHorizontalReversed() {
+	var goalsContainer = document.getElementById("goals");
+
+	if (goalsContainer) {
+		goalsContainer.scrollBy({
+			left: -goalsContainer.clientWidth, // Negative value to scroll left
+			behavior: "smooth", // Adding smooth scrolling behavior
+		});
+	}
+}
+
+/**======================
+ *    SCROLL HOME
+ *========================**/
+var scrollHomeButton = document.querySelector(".scrollhome");
+scrollHomeButton.addEventListener("click", scrollHome);
+
+function scrollHome() {
+	var goalsContainer = document.getElementById("goals");
+
+	if (goalsContainer) {
+		goalsContainer.scrollBy({
+			left: -2 * goalsContainer.clientWidth, // Scroll 2 times the negative width
+			behavior: "smooth", // Adding smooth scrolling behavior
+		});
+	}
+}
